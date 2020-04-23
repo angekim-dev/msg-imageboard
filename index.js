a323fb147805c19fd9eddd0b96946b32c00d5989
ANGELIKA KIM <alika852@gmail.com>
2020-04-23 11:33:31 +0200

    code after lecture on imageboard

--- index.js
+++ index.js
@@ -4,6 +4,30 @@ const db = require("./db");
 
 app.use(express.static("public"));
 
+////////////// IMAGE UPLOAD BOILERPLATE ///////////////
+const multer = require("multer");
+const uidSafe = require("uid-safe");
+const path = require("path");
+
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function (uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    },
+});
+
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152,
+    },
+});
+////////////////////////////////////////////
+
 // let cities = [
 //     { name: "Berlin", country: "DE" },
 //     { name: "Sheffield", country: "UK" },
@@ -11,16 +35,34 @@ app.use(express.static("public"));
 // ];
 
 app.get("/images", (req, res) => {
+    // here /images relates to axios, not the url
+    // because SPA single page application
     console.log("images route has been hit");
     return db
         .selectImage()
         .then((result) => {
-            console.log("***result of insertImage", result);
+            // console.log("***result of insertImage", result);
             res.json(result.rows);
         })
         .catch((err) => {
             console.log("Error in insertImage: ", err);
         });
+});
+
+app.post("upload", uploader.single("file"), (req, res) => {
+    console.log("file: ", req.file); // file we just uploaded
+    console.log("input: ", req.body); // input fields from the client
+
+    if (req.file) {
+        // you'll want to eventually make a db insert here for all the info
+        res.json({
+            success: true,
+        });
+    } else {
+        res.json({
+            success: false,
+        });
+    }
 });
 
 app.listen(8080, () => console.log("IB server is listening..."));
--- public/js/script.js
+++ public/js/script.js
@@ -5,25 +5,56 @@
         // el - represents which element in our html will have access tp our Vue code
         el: "#main",
         data: {
-            name: "msg",
-            seen: true,
             images: [],
+            title: "",
+            description: "",
+            username: "",
+            file: null,
         }, // data ends
         mounted: function () {
             // console.log("my vue has MOUNTED!");
 
             // console.log("this OUTSIDE axios", this);
             var self = this;
-            axios.get("/images").then(function (response) {
-                // console.log("response from /cities", response.data);
-                // console.log("this INSIDE axios", self);
-                self.images = response.data;
-            });
+            axios
+                .get("/images")
+                .then(function (response) {
+                    // console.log("response from /cities", response.data);
+                    // console.log("this INSIDE axios", self);
+                    self.images = response.data;
+                })
+                .catch(function (err) {
+                    console.log("error in GET /images: ", err);
+                });
         }, // mounted ends
-        // methods: {
-        //     myFunction: function () {
-        //         console.log("myFunction is running!");
-        //     },
-        // },
+        methods: {
+            handleClick: function (e) {
+                e.preventDefault();
+                console.log("data properties: ", this);
+
+                var formData = new FormData();
+                //use it because there is a file, normally would create an object
+                formData.append("title: ", this.title);
+                formData.append("description: ", this.description);
+                formData.append("username: ", this.username);
+                formData.append("file: ", this.file);
+                // if console.log formData, the result will be an empty object, but iz is still right
+
+                axios
+                    .post("/upload", formData)
+                    .then(function (resp) {
+                        console.log("resp from POST /upload: ", resp);
+                    })
+                    .catch(function (err) {
+                        console.log("error in POST /upload: ", err);
+                    });
+            },
+
+            handleChange: function (e) {
+                console.log("handleChange is running");
+                console.log("file: ", e.target.files[0]);
+                this.file = e.target.files[0];
+            },
+        },
     });
 })();
